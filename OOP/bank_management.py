b1350a21c060ca881001ac70be6e2300ee408ac6
Shobuj Das <dasshobuj@gmail.com>
2026-07-29 20:53:49 +0600

    banking/account search and remove

--- OOP/bank_management.py
+++ OOP/bank_management.py
@@ -89,3 +89,53 @@
 # display_transaction_history(account_number)
 #
 # ============================================================
+
+
+class BankAccount:
+    def __int__(self, account_number, account_holder, balance, transaction_history):
+        self.acc_number = account_number
+        self.acc_holder = self.acc_holder
+        self.balance = balance
+        self.acc_transaction_history = transaction_history
+
+    def __str__(self):
+        return(
+            f"Account Number: {self.acc_number} |"
+            f"Account Holder: {self.acc_holder} |"
+            f"Balance: {self.balance}"
+        )
+
+class Bank:
+    def __init__(self):
+        self.acc_list = []
+
+    def add_account(self,account):
+        self.acc_list.append(account)
+
+    def remove_account(self, account_number):
+        acc = self.search_account(account_number)
+        if acc is not None:
+            self.acc_list.remove(acc)
+            print("Account removed succesfully")
+
+            
+    def search_account(self, account_number):
+        if self.acc_list is None:
+            print("No account found")
+            return None
+        for account in self.acc_list:
+            if account.acc_number == account_number:
+                print("Acccount found")
+                return account
+        print("Account not found")
+        return None
+#
+# deposit(account_number, amount)
+#
+# withdraw(account_number, amount)
+#
+# transfer(from_account, to_account, amount)
+#
+# display_account(account_number)
+#
+# display_all_accounts()
